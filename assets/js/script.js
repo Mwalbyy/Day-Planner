d430751fe624f91271dff38a49f2449eeeb043c6
Marcus Walby <marcuswalby24@gmail.com>
2022-02-26 13:32:15 -0500

    Cleaned code got pulling items out of storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,15 +35,7 @@ function Clock() {
   });
 }
 
-$("#hour8 .description").val(localStorage.getItem("hour8"));
-$("#hour9 .description").val(localStorage.getItem("hour9"));
-$("#hour10 .description").val(localStorage.getItem("hour10"));
-$("#hour11 .description").val(localStorage.getItem("hour11"));
-$("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour13 .description").val(localStorage.getItem("hour13"));
-$("#hour14 .description").val(localStorage.getItem("hour14"));
-$("#hour15 .description").val(localStorage.getItem("hour15"));
-$("#hour16 .description").val(localStorage.getItem("hour16"));
-$("#hour17 .description").val(localStorage.getItem("hour17"));
-
+[1,2,3,4,5,6,7,8,9].forEach(hour => {
+  $(`#hour${hour} .description`).val(localStorage.getItem(`hour${hour}`));
+})
 Clock();
