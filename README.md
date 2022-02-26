57fef9dfa99a6d6d5fc44d97a42b44383e6999ee
Marcus Walby <marcuswalby24@gmail.com>
2022-02-26 13:25:57 -0500

    finished site

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Day-Planner
+# How to enter data the planner
+### The user can click on the time slot where they want to add something to their schedule and type out what ever they want.
+# How to save your items
+### The user can then save the item by clicking the save icon, making the info on the page remain even after refreshing to page.
+# Features
+### The time slots change color depending on whether the event passed, is coming up, or is going on currently. The Site also includes the date and time on the header.
+# Picture of site
+[image of site](./assets/images/Capture.PNG)
+
+### Link to site
+[Here](https://mwalbyy.github.io/Day-Planner/)
---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+let todayDay = moment().format("dddd, MMM Do YYYY");
+let timeNow = moment().format("h:mm A");
+$("#currentDay").html(todayDay);
+
+$("#currentTime").html(timeNow);
+
+$(document).ready(function () {
+  $(".saveBtn").on("click", function () {
+    console.log("Hello");
+    let text = $(this).siblings(".description").val();
+    let time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, text);
+  });
+});
+
+function Clock() {
+  let TimeATM = moment().hour();
+  $(".time-block").each(function () {
+    let TimeBlock = parseInt($(this).attr("id").split("hour")[1]);
+
+    if (TimeBlock < TimeATM) {
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+      $(this).addClass("past");
+    } else if (TimeBlock === TimeATM) {
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      $(this).addClass("future");
+    }
+  });
+}
+
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+
+Clock();
